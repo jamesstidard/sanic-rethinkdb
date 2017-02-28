356fc47c8bb77c9db2e2c02d2aaa13d97059e868
jamesstidard <jamesstidard@gmail.com>
2017-02-28 20:27:43 +0000

    init

---
+++ sanic_rethinkdb/__init__.py
@@ -0,0 +1,44 @@
+import os
+import asyncio
+
+from functools import partial
+
+import rethinkdb as r
+
+r.set_loop_type('asyncio')
+
+
+class RethinkDB:
+
+    def __init__(self, app=None):
+        self.app = app
+        self._connection_maker = r.connect
+        self._connections = {}
+
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.config.setdefault('RETHINKDB_HOST', 'localhost')
+        app.config.setdefault('RETHINKDB_PORT', '28015')
+        app.config.setdefault('RETHINKDB_AUTH', '')
+        app.config.setdefault('RETHINKDB_DB', 'test')
+
+        @app.listener('after_server_stop')
+        async def teardown():
+            closers = [c.close for c in self._connections]
+            await asyncio.wait(closers)
+
+        self._connection_maker = partial(r.connect,
+                                         auth_key=app.config.RETHINKDB_AUTH,
+                                         host=app.config.RETHINKDB_HOST,
+                                         port=app.config.RETHINKDB_PORT,
+                                         db=app.config.RETHINKDB_DB)
+
+    async def connection(self):
+        pid = os.getpid()
+        if pid in self._connections:
+            return self._connections[pid]
+        else:
+            connection = await self._connection_maker()
+            return self._connections.setdefault(pid, connection)
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='sanic_rethinkdb',
+    version='0.1',
+    packages=['sanic_rethinkdb'],
+    url='https://github.com/jamesstidard/sanic-rethinkdb',
+    license='MIT',
+    author='James Stidard',
+    author_email='jamesstidard@gmail.com',
+    description='A simple RethinkDB extension for Sanic',
+    keywords='sanic database rethinkdb extension',
+    install_requires=[
+        'rethinkdb',
+        'sanic'])
